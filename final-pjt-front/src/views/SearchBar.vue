83b024c450f49cb70800cd6bdb93e2bb57552a66
권예빈 <kwonay11@naver.com>
2021-06-15 10:21:03 +0900

    SearchBar

--- final-pjt-front/src/views/SearchBar.vue
+++ final-pjt-front/src/views/SearchBar.vue
@@ -1,63 +1,87 @@
 <template>
- <div class="card bg-dark">
-     <h2>검색어 : {{$route.query.keyword}}</h2>
-     <div >
-       <!-- keyword와 영화제목이 같은것의 무비카드 보여주기 -->
-
-     
-    <h3>영화 제목: {{ keyword }}</h3>
-
-        <MovieCard
-          :movie="movie"
-        />
+ <div class="card bg-dark d-flex justify-content-center align-items-center">
+     <h2>검색어 : {{ keyword }}</h2>
+     <!-- 자료가 없을 때-->
+    <div v-if="result.length === 0">
+    <h1>해당 영화에 대한 정보가 없습니다.</h1>
     </div>
+    <div v-else>
+
+      <h3>{{ result }}</h3>
+      
+      <MovieCard />
+      
+      
+       
+      </div>
+       
+    
+
  </div>
 </template>
 
 <script>
 import axios from "axios"
 const SERVER_URL = process.env.VUE_APP_SERVER_URL
-
+import { Glide, GlideSlide } from 'vue-glide-js'
 import MovieCard from "@/components/MovieCard"
 
 export default {
   name: 'SearchBar',
   components: {
     MovieCard,
-   
+    [Glide.name]: Glide,
+    [GlideSlide.name]: GlideSlide,
   },
   data: function () {
     return {
       keyword: '',
       movies: [],
-      result: []
+      result: [],
+      
     }
   },
   methods: {
       getMovies: function () {
       axios.get(`${SERVER_URL}/movies/`)
-        .then(res => {
+        .then((res) => {
           this.movies = res.data
-        })
-        .catch(err => {
-          console.log(err)
-        })
-      },
-      findMovie: function () {
-      for(var i=0; i<this.movies.length; i++) {
-        if (this.keyword in this.movies[i].title) {
+          for(var i=0; i<this.movies.length; i++) {
+        if (this.keyword === this.movies[i].title) {
           this.result.push(this.movies[i])
+          console.log(this.movies[i])
+          console.log('에러안남')
+
+          
+          
         }
       }
-      console.log(this.result)
-    },
+         
+        })
+        .catch((err) => {
+          console.log(err)
+          console.log('에러')
+        })
+      },
+    //   findMovie: function () {
+    //   for(var i=0; i<this.movies.length; i++) {
+    //     if (this.keyword === this.movies[i].title) {
+    //       this.result.push(this.movies[i])
+    //       console.log(this.movies[i])
+          
+          
+    //     }
+    //   }
+    //   console.log(this.result) //값이 안들어감
+    //   console.log('영화찾아짐')
+    // },
    
   },
   created: function () {
-    this.keyword = this.$route.query.keyword
-    console.log(this.keyword)
+    this.keyword = this.$route.query.keyword //검색창에서 받아옴
+    // console.log(this.keyword)
     this.getMovies()
-    this.findMovie()
+    // this.findMovie()
 }
 }
 </script>
